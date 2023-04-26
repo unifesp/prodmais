141e3056974e4be1542ab13273351233b5f80743
Tiago Murakami <trmurakami@gmail.com>
2023-04-26 16:16:24 -0300

    Openalex

---
+++ tools/openalex_get_record.php
@@ -0,0 +1,46 @@
+<?php
+
+// Set directory to ROOT
+chdir('../');
+// Include essencial files
+require 'inc/functions.php';
+
+/* Exibir erros */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(1);
+
+
+$query["query"]["query_string"]["query"] = '_exists_:doi doi:1* -_exists_:openalex';
+
+$params = [];
+$params["index"] = $index;
+$params["body"] = $query;
+
+$cursorTotal = $client->count($params);
+$total = $cursorTotal["count"];
+
+$params["size"] = $_GET["size"];
+
+$cursor = $client->search($params);
+
+echo "Resultado: $total";
+
+foreach ($cursor["hits"]["hits"] as $r) {
+    // //print("<pre>".print_r($r, true)."</pre>");
+    // //print("<pre>".print_r($r["_source"]["doi"], true)."</pre>");    
+    $openalex_result = openalexAPI($r["_source"]["doi"]);
+    unset($openalex_result['abstract_inverted_index']);
+    // //print("<pre>".print_r($openalex_result, true)."</pre>");
+    if (empty($openalex_result)) {
+        $body["doc"]["openalex"]['empty'] = true;
+    } else {
+        $body["doc"]["openalex"] = $openalex_result;
+    }
+    $body["doc_as_upsert"] = true;
+    // //print("<pre>".print_r($body, true)."</pre>");
+    $upsert_openalex = Elasticsearch::update($r["_id"], $body);
+    // print("<pre>" . print_r($upsert_openalex, true) . "</pre>");
+    ob_flush();
+    flush();
+}
