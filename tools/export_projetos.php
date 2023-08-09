6c859c811c9543b25007f5d2be19f964da1b9779
Tiago Murakami <trmurakami@gmail.com>
2023-08-08 21:09:47 -0300

    export projetos

--- tools/export_projetos.php
+++ tools/export_projetos.php
@@ -23,6 +23,33 @@
                 $projectArray[4] = $projeto_de_pesquisa['attributes']['DESCRICAO-DO-PROJETO'];
             }
         }
+        $financiadoresArray = [];
+        if(isset($project['FINANCIADORES-DO-PROJETO'])) {
+            //echo "<pre>".print_r($project['FINANCIADORES-DO-PROJETO'], true)."</pre>";
+            if (isset($project['FINANCIADORES-DO-PROJETO']['FINANCIADOR-DO-PROJETO']['attributes'])){
+                $financiadoresArray[] = $project['FINANCIADORES-DO-PROJETO']['FINANCIADOR-DO-PROJETO']['attributes']['NOME-INSTITUICAO'];
+                $projectArray[5] = implode("|", $financiadoresArray);
+                unset($financiadoresArray);
+            } else {
+                foreach ($project['FINANCIADORES-DO-PROJETO']['FINANCIADOR-DO-PROJETO'] as $key => $financiador) {
+                    $financiadoresArray[] = $financiador['attributes']['NOME-INSTITUICAO'];
+                }
+                $projectArray[5] = implode("|", $financiadoresArray);
+                unset($financiadoresArray);
+            }
+        }
+        if (isset($project['EQUIPE-DO-PROJETO'])) {
+            $equipeArray = [];
+            foreach ($project['EQUIPE-DO-PROJETO']['INTEGRANTES-DO-PROJETO'] as $key => $integrante) {
+                if (isset($integrante['NOME-COMPLETO'])) {
+                    $equipeArray[] = $integrante['NOME-COMPLETO'];
+                } else {
+                    $equipeArray[] = $integrante['attributes']['NOME-COMPLETO'];
+                }
+            }
+            $projectArray[6] = implode("|", $equipeArray);
+            unset($equipeArray);
+        }
         return $projectArray;
     }
 
@@ -36,7 +63,7 @@
     $params = [];
     $query["query"]["query_string"]["query"] = "*";
     $params["index"] = $index_cv;
-    $params["size"] = 2;
+    $params["size"] = 10;
     $params["_source"] = ["atuacoes_profissionais"];
     //$params["fields"] = ['atuacoes_profissionais'];
     $params["scroll"] = "30s";
@@ -45,7 +72,7 @@
     $cursor = $client->search($params);
     $total = $cursor["hits"]["total"];
 
-    $content[] = "id\ttítulo\tanoinicio\tanofim\tsituacao\tdescricaodoprojeto";
+    $content[] = "id\ttítulo\tanoinicio\tanofim\tsituacao\tdescricaodoprojeto\tfinanciadores\tequipe";
 
     foreach ($cursor["hits"]["hits"] as $r) {
         foreach ($r['_source']['atuacoes_profissionais'] as $key => $atuacoes_profissionais) {
@@ -83,20 +110,49 @@
         }
     }
 
+    while (isset($cursor['hits']['hits']) && count($cursor['hits']['hits']) > 0) {
+        $scroll_id = $cursor['_scroll_id'];
+        $cursor = $client->scroll(
+            [
+                "scroll_id" => $scroll_id,
+                "scroll" => "30s"
+            ]
+        );
 
-
-
-    // while (isset($cursor['hits']['hits']) && count($cursor['hits']['hits']) > 0) {
-    //     $scroll_id = $cursor['_scroll_id'];
-    //     $cursor = $client->scroll(
-    //         [
-    //             "scroll_id" => $scroll_id,
-    //             "scroll" => "30s"
-    //         ]
-    //     );
-
-    //     foreach ($cursor["hits"]["hits"] as $r) {
-    //         $content[] = createTableDSpace($r);
-    //     }
-    // }
+        foreach ($cursor["hits"]["hits"] as $r) {
+            foreach ($r['_source']['atuacoes_profissionais'] as $key => $atuacoes_profissionais) {
+                foreach ($atuacoes_profissionais as $key => $atuacao_profissional) {
+                    $dataJson = str_replace('@', '', json_encode($atuacao_profissional));
+                    $atuacao_profissional = json_decode($dataJson, true);
+                    $projetoArray = [];
+                    if (isset($atuacao_profissional['ATIVIDADES-DE-PARTICIPACAO-EM-PROJETO']['PARTICIPACAO-EM-PROJETO'])) {
+                        if (isset($atuacao_profissional['ATIVIDADES-DE-PARTICIPACAO-EM-PROJETO']['PARTICIPACAO-EM-PROJETO']['PROJETO-DE-PESQUISA'])) {
+                            $projetoArray = processProject($atuacao_profissional['ATIVIDADES-DE-PARTICIPACAO-EM-PROJETO']['PARTICIPACAO-EM-PROJETO']['PROJETO-DE-PESQUISA']);
+                            $content[] = $r['_id']."\t".implode("\t", $projetoArray);
+                            unset($projetoArray);
+                        } else {
+                            foreach ($atuacao_profissional['ATIVIDADES-DE-PARTICIPACAO-EM-PROJETO']['PARTICIPACAO-EM-PROJETO'] as $key => $participacao_em_projeto) {
+                                
+                                if (isset($participacao_em_projeto['PROJETO-DE-PESQUISA']['attributes'])) {
+                                    $projetoArray = processProject($participacao_em_projeto['PROJETO-DE-PESQUISA']);
+                                } else {
+                                    if (isset($participacao_em_projeto['PROJETO-DE-PESQUISA'])) {
+                                        foreach ($participacao_em_projeto['PROJETO-DE-PESQUISA'] as $key => $projeto_de_pesquisa) {
+                                            $projetoArray = processProject($projeto_de_pesquisa);
+                                            $content[] = $r['_id']."\t".implode("\t", $projetoArray);
+                                            unset($projetoArray);
+                                        }
+                                    } else {}
+                                }
+                                if (isset($projetoArray)) {
+                                    $content[] = $r['_id']."\t".implode("\t", $projetoArray);
+                                    unset($projetoArray);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     echo implode("\n<br/>", $content);
