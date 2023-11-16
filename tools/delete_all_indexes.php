74ba45651c7132bf882d63715d97dfe42fb2d8a6
Tiago Murakami <trmurakami@gmail.com>
2023-11-16 08:18:39 -0300

    Delete all index

--- tools/delete_all_indexes.php
+++ tools/delete_all_indexes.php
@@ -2,49 +2,19 @@
 
 require '../inc/config.php';
 
-$url = "localhost:9200/prodmais";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-$result = json_decode($result);
-//var_dump($result);
-curl_close($ch);
+$params = ['index' => $index];
+$response = $client->indices()->delete($params);
 
 
-$url = "localhost:9200/prodmaiscv";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-$result = json_decode($result);
-//var_dump($result);
-curl_close($ch);
+$params = ['index' => $index_cv];
+$response = $client->indices()->delete($params);
 
-$url = "localhost:9200/prodmaisppg";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-$result = json_decode($result);
-//var_dump($result);
-curl_close($ch);
+$params = ['index' => $index_ppg];
+$response = $client->indices()->delete($params);
 
-$url = "localhost:9200/prodmaisprojetos";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-$result = json_decode($result);
-//var_dump($result);
-curl_close($ch);
+
+$params = ['index' => $index_projetos];
+$response = $client->indices()->delete($params);
+
 
 header("Location: $url_base");
