d5c3476670b89e082ff9d97d0f48910f42bd1604
Tiago Murakami <trmurakami@gmail.com>
2024-01-03 03:35:11 -0300

    Openalex

--- tools/apis.php
+++ tools/apis.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
 <style>
-.form-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-}
+    .form-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
 </style>
 
 
@@ -31,7 +31,7 @@ if (isset($_POST['submit'])) {
     if ($_POST['username'] == $username && $_POST['password'] == $password) {
 ?>
 
-<?php
+        <?php
 
         // Get status of APIs
 
@@ -85,60 +85,62 @@ if (isset($_POST['submit'])) {
 
         ?>
 
-<body class="c-wrapper-body">
-    <main class="c-wrapper-container">
-        <div class="c-wrapper-paper">
-            <div class="c-wrapper-inner">
-                <h1 class="t t-h1"><?php echo $branch; ?> - Status de APIs</h1>
+        <body class="c-wrapper-body">
+            <main class="c-wrapper-container">
+                <div class="c-wrapper-paper">
+                    <div class="c-wrapper-inner">
+                        <h1 class="t t-h1"><?php echo $branch; ?> - Status de APIs</h1>
 
-                <h2 class='t t-h3'>Total de registros no banco de dados: <?php echo $total_records; ?></h2>
+                        <h2 class='t t-h3'>Total de registros no banco de dados: <?php echo $total_records; ?></h2>
 
-                <h2 class='t t-h3'>Total de registros no banco de dados com DOI: <?php echo $total_records_with_DOI; ?>
-                </h2>
+                        <h2 class='t t-h3'>Total de registros no banco de dados com DOI: <?php echo $total_records_with_DOI; ?>
+                        </h2>
 
-                <h1 class="t t-h1">OpenAlex</h1>
+                        <h1 class="t t-h1">OpenAlex</h1>
 
-                <h2 class='t t-h3'>Total de registros no banco de dados com DOI e OpenAlex:
-                    <?php echo $total_records_with_OpenAlex; ?>, faltando
-                    <?php echo $total_records_with_DOI - $total_records_with_OpenAlex; ?>
-                </h2>
+                        <h2 class='t t-h3'>Total de registros no banco de dados com DOI e OpenAlex:
+                            <?php echo $total_records_with_OpenAlex; ?>, faltando
+                            <?php echo $total_records_with_DOI - $total_records_with_OpenAlex; ?>
+                        </h2>
 
-                <h2 class='t t-h3'>Total de registros no banco de dados sem DOI e sem OpenAlex:
-                    <?php echo $total_records_with_OpenAlex_without_DOI; ?></h2>
-                <hr />
+                        <h2 class='t t-h3'>Total de registros no banco de dados sem DOI e sem OpenAlex:
+                            <?php echo $total_records_with_OpenAlex_without_DOI; ?></h2>
+                        <hr />
 
-                <h2 class="t t-h3">Enriquecimento de registros com OpenAlex</h2>
-                <ul>
-                    <li><a href="openalex_get_record.php?size=10">Coletar 10 registros no Openalex com DOI (É
-                            necessário
-                            repetir este procedimento até zerar a quantidade de registros)</a></li>
-                    <li><a href="openalex_get_doi.php?size=5">Tentar obter o DOI no OpenAlex de registros que não
-                            possuem
-                            DOI, com base no título.
-                            (É necessário repetir este procedimento até zerar a quantidade de registros, mas esse é
-                            um
-                            procedimento demorado, que deve ser realizado com poucos registros de cada vez)</a></li>
-                </ul>
+                        <h2 class="t t-h3">Enriquecimento de registros com OpenAlex</h2>
+                        <ul>
+                            <li><a href="openalex_get_record.php?size=10">Coletar 10 registros no Openalex com DOI (É
+                                    necessário repetir este procedimento até zerar a quantidade de registros)</a></li>
+                            <li><a href="openalex_get_record_with_citations.php?size=10">Coletar 10 registros no Openalex com
+                                    DOI e coletar as citações (É
+                                    necessário repetir este procedimento até zerar a quantidade de registros)</a></li>
+                            <li><a href="openalex_get_doi.php?size=5">Tentar obter o DOI no OpenAlex de registros que não
+                                    possuem
+                                    DOI, com base no título.
+                                    (É necessário repetir este procedimento até zerar a quantidade de registros, mas esse é
+                                    um
+                                    procedimento demorado, que deve ser realizado com poucos registros de cada vez)</a></li>
+                        </ul>
 
-                <h1 class="t t-h1">Aurora SDG</h1>
+                        <h1 class="t t-h1">Aurora SDG</h1>
 
-                <h2 class='t t-h3'>Total de registros no banco de dados com Aurora SDG:
-                    <?php echo $total_records_with_AuroraSDG; ?>
-                </h2>
-                <h2 class="t t-h3">Enriquecimento de registros com Aurora SDG</h2>
-                <ul>
-                    <li>
-                        <a href="aurorasdg_get.php?size=10">Coletar 10 registros no Aurora SDG (É
-                            necessário repetir este procedimento até zerar a quantidade de registros)
-                        </a>
-                    </li>
-                </ul>
+                        <h2 class='t t-h3'>Total de registros no banco de dados com Aurora SDG:
+                            <?php echo $total_records_with_AuroraSDG; ?>
+                        </h2>
+                        <h2 class="t t-h3">Enriquecimento de registros com Aurora SDG</h2>
+                        <ul>
+                            <li>
+                                <a href="aurorasdg_get.php?size=10">Coletar 10 registros no Aurora SDG (É
+                                    necessário repetir este procedimento até zerar a quantidade de registros)
+                                </a>
+                            </li>
+                        </ul>
 
 
-            </div>
-        </div>
-    </main>
-</body>
+                    </div>
+                </div>
+            </main>
+        </body>
 
 </html>
 
