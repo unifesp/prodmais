6435f382c2d08bacbb78f668cf24e800582d22c6
Tiago Murakami <trmurakami@gmail.com>
2024-02-10 08:47:40 -0300

    Footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,25 +2,26 @@
 
 <footer class="sitefooter">
 
-  <?php if($instituicao == "UNIFESP"): ?>
+    <?php if ($instituicao == "UNIFESP") : ?>
 
-  <div class="sitefooter-contacts">
-    <a class="sitefooter-link" target="_blank" href="https://atendimento.unifesp.br/">
-      Fale conosco</a>
-    <!--<a class="sitefooter-link" target="_blank" href="https://atendimento.unifesp.br/">
+    <div class="sitefooter-contacts">
+        <a class="sitefooter-link" target="_blank" href="https://atendimento.unifesp.br/">
+            Fale conosco</a>
+        <!--<a class="sitefooter-link" target="_blank" href="https://atendimento.unifesp.br/">
       Política de privacidade</a> -->
-    <a class="sitefooter-link" target="_blank" href="https://forms.gle/2QRcqg2YfxMvEqVX9">
-      Relate erros</a>
-  </div>
+        <a class="sitefooter-link" target="_blank" href="https://forms.gle/2QRcqg2YfxMvEqVX9">
+            Relate erros</a>
+    </div>
 
-  <?php else: ?>
+    <?php else : ?>
 
     <a class="sitefooter-link" href="inclusao.php">Inclusão</a>
 
-  <?php endif ?>
+    <?php endif ?>
 
-  <p class="developed-by">Software livre desenvolvido originalmente pela Universidade Federal de São Paulo</p>
-  <a href="https://unifesp.br/" target="_blank" class="sitefooter-link" title="Visite o site da Unifesp">
-    <i class="i i-unifesp" alt="Logo da Unifesp"></i>
-  </a>
+    <p class="developed-by"><a href="https://github.com/unifesp/prodmais">Prodmais</a> é um Software livre desenvolvido
+        originalmente pela Universidade Federal de São Paulo</p>
+    <a href="https://unifesp.br/" target="_blank" class="sitefooter-link" title="Visite o site da Unifesp">
+        <i class="i i-unifesp" alt="Logo da Unifesp"></i>
+    </a>
 </footer>
