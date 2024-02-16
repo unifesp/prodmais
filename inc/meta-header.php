d71d431087aac079d2e368d60599872a648e4c96
Tiago Murakami <trmurakami@gmail.com>
2024-02-16 15:07:33 -0300

    Correção css

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="<?php echo $url_base; ?>/inc/images/favicon-64x.png" type="image/x-icon">
 <link rel="stylesheet" href="inc/sass/main.css" />
-<link rel="stylesheet" href="/inc/sass/main.css" />
+<link rel="stylesheet" href="/prodmais/inc/sass/main.css" />
 
 <?php if (file_exists('inc/js/vue.js')) : ?>
 <!-- Development -->
